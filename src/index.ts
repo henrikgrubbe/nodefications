fd9354c0042b0e993dae468a5d4dee28ad59fcbd
Henrik Herbst Sørensen <hhs19942@gmail.com>
2021-03-18 08:12:46 +0100

    Add improved handling of missing environment variables

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ import {notificationRouter} from './notification/notification.controller';
 
 dotenv.config();
 
-/* Variables */
-if (!process.env.PORT) {
-    process.exit(1);
-}
+const envVars: Record<string, string | undefined> = {
+    'PORT': process.env.PORT,
+};
+checkEnvVars(envVars);
+
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
@@ -24,3 +25,17 @@ app.use('/api/notifications', notificationRouter);
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+function checkEnvVars(envVars: Record<string, string | undefined>) {
+    let allEnvVarsFound = true;
+    for (const envVar of Object.keys(envVars)) {
+        if (envVars[envVar] === undefined) {
+            console.warn(`Environment variable ${envVar} not found`);
+            allEnvVarsFound = false;
+        }
+    }
+    if (!allEnvVarsFound) {
+        console.error("Not all environment variables where found, quitting")
+        process.exit(1);
+    }
+}
